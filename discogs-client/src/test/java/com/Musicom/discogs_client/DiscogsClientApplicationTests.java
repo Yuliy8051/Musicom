f1b946ab4cfcbcf52b1bbfefc1549db671785c43
“Denys <denn8051@gmail.com>
2025-01-11 23:44:15 +0100

    add client for external api

---
+++ discogs-client/src/test/java/com/Musicom/discogs_client/DiscogsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Musicom.discogs_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscogsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
