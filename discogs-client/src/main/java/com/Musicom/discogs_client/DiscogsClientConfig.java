83bd47098d8914df630b972e6d84fff787317a95
“Denys <denn8051@gmail.com>
2025-01-12 02:54:47 +0100

    fix bug with data loading from .properties file

--- discogs-client/src/main/java/com/Musicom/discogs_client/DiscogsClientConfig.java
+++ discogs-client/src/main/java/com/Musicom/discogs_client/DiscogsClientConfig.java
@@ -3,8 +3,11 @@ package com.Musicom.discogs_client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestClient;
 
 @Configuration
+@PropertySource("classpath:secretApplication.properties")
 public class DiscogsClientConfig {
     @Bean
     public DiscogsClientUrlBuilderProvider discogsClientUrlBuilderProvider(
@@ -12,5 +15,9 @@ public class DiscogsClientConfig {
             @Value("${discogs.api.consumer_secret}") String consumerSecret,
             @Value("${discogs.api.host}") String host) {
         return new DiscogsClientUrlBuilderProvider(consumerKey, consumerSecret, host);
+    }
+    @Bean
+    public RestClient restClient() {
+        return RestClient.create();
     }
 }
